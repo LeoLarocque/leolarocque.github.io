efcb5c83431abb23b0a01eb922489bf2af2befe1
Watson63 <149442371+Watson63@users.noreply.github.com>
2023-11-30 13:40:50 -0700

    Add files via upload

---
+++ volleyballjava.js
@@ -0,0 +1,43 @@
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("Table1");
+    switching = true;
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+      for (i = 1; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("TD")[2];
+        y = rows[i + 1].getElementsByTagName("TD")[2];
+        if (Number(x.innerHTML) < Number(y.innerHTML)) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
+  }
+  var number_of_games = 16;
+  function tablebrains() {
+    var table = document.getElementById("Table1");
+    var rows = table.rows;
+  
+    for (var i = 1; i < rows.length; i++) {
+      var valueCell = rows[i].getElementsByClassName("wins")[0];
+      var lossesCell = rows[i].getElementsByClassName("losses")[0];
+      var percentageCell = rows[i].getElementsByClassName("percentage")[0];
+  
+      var value = Number(valueCell.innerHTML);
+      var losses = number_of_games - value;
+      var percentage = (value / number_of_games) * 100;
+  
+      percentageCell.innerHTML = percentage.toFixed(2) + "%";
+      lossesCell.innerHTML = losses.toFixed (0);
+    }
+  }
+  
+  sortTable();
+  tablebrains();
